69960b0e7488140dbb65649f2420d595f150a41d
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-08-06 15:04:53 +1000

    Use HTTPS

--- ckanext/ga_report/public/scripts/application.js
+++ ckanext/ga_report/public/scripts/application.js
@@ -762,7 +762,7 @@ CKAN.View.ResourceAddUpload = Backbone.View.extend({
         self.updateFormData(self.key);
       },
       send: function(e, data) {
-        self.setMessage(CKAN.Strings.uploadingFile +' <img src="http://assets.okfn.org/images/icons/ajaxload-circle.gif" class="spinner" />');
+        self.setMessage(CKAN.Strings.uploadingFile +' <img src="https://assets.okfn.org/images/icons/ajaxload-circle.gif" class="spinner" />');
       },
       done: function(e, data) {
         self.onUploadComplete(self.key);
@@ -800,7 +800,7 @@ CKAN.View.ResourceAddUpload = Backbone.View.extend({
 
   updateFormData: function(key) {
     var self = this;
-    self.setMessage(CKAN.Strings.checkingUploadPermissions + ' <img src="http://assets.okfn.org/images/icons/ajaxload-circle.gif" class="spinner" />');
+    self.setMessage(CKAN.Strings.checkingUploadPermissions + ' <img src="https://assets.okfn.org/images/icons/ajaxload-circle.gif" class="spinner" />');
     self.el.find('.fileinfo').text(key);
     $.ajax({
       url: CKAN.SITE_URL + '/api/storage/auth/form/' + key,
@@ -1468,7 +1468,7 @@ CKAN.DataPreview = function ($, my) {
   // then passes the constructed Dataset, the constructed View, and the
   // reclineState into the DataExplorer constructor.
   my.loadEmbeddedPreview = function(resourceData, reclineState) {
-    my.$dialog.html('<h4>Loading ... <img src="http://assets.okfn.org/images/icons/ajaxload-circle.gif" class="loading-spinner" /></h4>');
+    my.$dialog.html('<h4>Loading ... <img src="https://assets.okfn.org/images/icons/ajaxload-circle.gif" class="loading-spinner" /></h4>');
 
     // Restore the Dataset from the given reclineState.
     var datasetInfo = _.extend({
@@ -1551,7 +1551,7 @@ CKAN.DataPreview = function ($, my) {
   //
   // Returns nothing.
   my.loadPreviewDialog = function(resourceData) {
-    my.$dialog.html('<h4>Loading ... <img src="http://assets.okfn.org/images/icons/ajaxload-circle.gif" class="loading-spinner" /></h4>');
+    my.$dialog.html('<h4>Loading ... <img src="https://assets.okfn.org/images/icons/ajaxload-circle.gif" class="loading-spinner" /></h4>');
 
     function showError(msg){
       msg = msg || CKAN.Strings.errorLoadingPreview;
